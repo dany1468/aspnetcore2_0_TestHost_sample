c519d5b97931d52b36cd54ee8a91a6ca2a5a68a6
dany1468 <dany1468@gmail.com>
2018-01-31 15:35:52 +0900

    Add Infrastracture code and Add PreserveCompilationContext setting to .csproj

---
+++ E2eTest/Infrastracture/MvcTestFixture.cs
@@ -0,0 +1,41 @@
+﻿using System.Globalization;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+
+namespace E2eTest.Infrastracture
+{
+    // This file is copied from following url.
+    // https://github.com/aspnet/Mvc/blob/dev/test/Microsoft.AspNetCore.Mvc.FunctionalTests/Infrastructure/MvcTestFixture.cs
+
+    public class MvcTestFixture<TStartup> : WebApplicationTestFixture<TStartup>
+         where TStartup : class
+    {
+        public MvcTestFixture()
+            : base(Path.Combine("test", "WebSites", typeof(TStartup).Assembly.GetName().Name))
+        {
+        }
+
+        protected MvcTestFixture(string solutionRelativePath)
+            : base(solutionRelativePath)
+        {
+        }
+
+        protected override TestServer CreateServer(WebHostBuilder builder)
+        {
+            var originalCulture = CultureInfo.CurrentCulture;
+            var originalUICulture = CultureInfo.CurrentUICulture;
+            try
+            {
+                CultureInfo.CurrentCulture = new CultureInfo("en-GB");
+                CultureInfo.CurrentUICulture = new CultureInfo("en-US");
+                return base.CreateServer(builder);
+            }
+            finally
+            {
+                CultureInfo.CurrentCulture = originalCulture;
+                CultureInfo.CurrentUICulture = originalUICulture;
+            }
+        }
+    }
+}
---
+++ E2eTest/Infrastracture/WebHostBuilderExtensions.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Microsoft.AspNetCore.Hosting;
+
+namespace E2eTest.Infrastracture
+{
+
+    // This file is copied from following url.
+    // https://github.com/aspnet/Hosting/blob/dev/src/Microsoft.AspNetCore.TestHost/WebHostBuilderExtensions.cs
+
+    public static class WebHostBuilderExtensions
+    {
+        public static IWebHostBuilder UseSolutionRelativeContentRoot(
+            this IWebHostBuilder builder,
+            string solutionRelativePath,
+            string solutionName = "*.sln")
+        {
+            return builder.UseSolutionRelativeContentRoot(solutionRelativePath, AppContext.BaseDirectory, solutionName);
+        }
+
+        public static IWebHostBuilder UseSolutionRelativeContentRoot(
+            this IWebHostBuilder builder,
+            string solutionRelativePath,
+            string applicationBasePath,
+            string solutionName = "*.sln")
+        {
+            if (solutionRelativePath == null)
+            {
+                throw new ArgumentNullException(nameof(solutionRelativePath));
+            }
+
+            if (applicationBasePath == null)
+            {
+                throw new ArgumentNullException(nameof(applicationBasePath));
+            }
+
+            var directoryInfo = new DirectoryInfo(applicationBasePath);
+            do
+            {
+                var solutionPath = Directory.EnumerateFiles(directoryInfo.FullName, solutionName).FirstOrDefault();
+                if (solutionPath != null)
+                {
+                    builder.UseContentRoot(Path.GetFullPath(Path.Combine(directoryInfo.FullName, solutionRelativePath)));
+                    return builder;
+                }
+
+                directoryInfo = directoryInfo.Parent;
+            }
+            while (directoryInfo.Parent != null);
+
+            throw new InvalidOperationException($"Solution root could not be located using application root {applicationBasePath}.");
+        }
+    }
+}
